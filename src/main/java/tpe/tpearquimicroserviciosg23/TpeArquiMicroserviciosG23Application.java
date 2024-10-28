2bc124dfb61226de869cef299c3a89779f6125af
Florencia Vivar <daianavivar93@gmail.com>
2024-10-28 11:52:16 -0300

    creada la estructura de microservicios basica

---
+++ src/main/java/tpe/tpearquimicroserviciosg23/TpeArquiMicroserviciosG23Application.java
@@ -0,0 +1,13 @@
+package tpe.tpearquimicroserviciosg23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpeArquiMicroserviciosG23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpeArquiMicroserviciosG23Application.class, args);
+    }
+
+}
---
+++ src/test/java/tpe/tpearquimicroserviciosg23/TpeArquiMicroserviciosG23ApplicationTests.java
@@ -0,0 +1,13 @@
+package tpe.tpearquimicroserviciosg23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpeArquiMicroserviciosG23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
